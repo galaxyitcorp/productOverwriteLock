3ff915df5d3e16f74852390d9e66af48b636a4e3
Alex Gorokhovsky <alex@galaxyitcorp.com>
2016-10-04 13:22:43 -0500

    Added attributes lock

--- app/code/local/HubCo/ProductOverwriteLock/Model/Source/Lockfields.php
+++ app/code/local/HubCo/ProductOverwriteLock/Model/Source/Lockfields.php
@@ -3,10 +3,12 @@
 class HubCo_ProductOverwriteLock_Model_Source_Lockfields extends Mage_Eav_Model_Entity_Attribute_Source_Abstract {
   public function getAllOptions() {
     $options = array();
+    $options[] = array('label' => 'None', 'value' => 'none');
     $options[] = array('label' => 'Name', 'value' => 'name');
     $options[] = array('label' => 'Description', 'value' => 'description');
     $options[] = array('label' => 'Short Description', 'value' => 'short_description');
     $options[] = array('label' => 'Price', 'value' => 'price');
+    $options[] = array('label' => 'Attributes (Color, size, etc.)', 'value' => 'attributes');
 
     return $options;
   }
