011e4f97e91acfa63e3a44e98581a6b8dfaadd98
lorman <devedge@c127h-thehub-web1.jetrails.io>
2017-02-09 15:26:37 -0600

    Added Image Locks

--- app/code/local/HubCo/ProductOverwriteLock/Model/Source/Lockfields.php
+++ app/code/local/HubCo/ProductOverwriteLock/Model/Source/Lockfields.php
@@ -10,6 +10,10 @@ class HubCo_ProductOverwriteLock_Model_Source_Lockfields extends Mage_Eav_Model_
     $options[] = array('label' => 'Price', 'value' => 'price');
     $options[] = array('label' => 'Attributes (Color, size, etc.)', 'value' => 'attributes');
     $options[] = array('label' => 'Google Active', 'value' => 'google_active');
+    $options[] = array('label' => 'Thumbnail Image', 'value' => 'thumbnail');
+    $options[] = array('label' => 'Small Image', 'value' => 'small_image');
+    $options[] = array('label' => 'Base Image', 'value' => 'image');
+    $options[] = array('label' => 'Image Gallery', 'value' => 'media_gallery');
 
     return $options;
   }
