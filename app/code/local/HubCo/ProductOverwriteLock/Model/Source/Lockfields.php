c4345f3b1c9b7dbaa54fa462ff6daacf20ba4c29
lorman <devedge@c127h-thehub-web1.jetrails.io>
2017-02-09 13:03:46 -0600

    Prod Overwrite

---
+++ app/code/local/HubCo/ProductOverwriteLock/Model/Source/Lockfields.php
@@ -0,0 +1,18 @@
+<?php
+
+class HubCo_ProductOverwriteLock_Model_Source_Lockfields extends Mage_Eav_Model_Entity_Attribute_Source_Abstract {
+  public function getAllOptions() {
+    $options = array();
+    $options[] = array('label' => 'None', 'value' => 'none');
+    $options[] = array('label' => 'Name', 'value' => 'name');
+    $options[] = array('label' => 'Description', 'value' => 'description');
+    $options[] = array('label' => 'Short Description', 'value' => 'short_description');
+    $options[] = array('label' => 'Price', 'value' => 'price');
+    $options[] = array('label' => 'Attributes (Color, size, etc.)', 'value' => 'attributes');
+    $options[] = array('label' => 'Google Active', 'value' => 'google_active');
+
+    return $options;
+  }
+}
+
+?>
